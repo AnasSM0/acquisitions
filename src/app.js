8eef1f5e837a6ab4a4643cebb6bfb0edc67e99dc
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-10-27 18:58:55 +0500

    Eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello from Acqusitions!');
 });
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
 export default app;
